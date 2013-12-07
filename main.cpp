e39efdaaa1f9cfd1e131870ff97798b83003cd75
Keng-ichi Ahagon <k.ahagon@n-3.so>
2013-12-07 19:05:52 +0900

    add Token class

---
+++ main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <stdio.h>
+#include "Token.h"
+
+
+int main(int argc, char *argv[]) {
+    
+    SSC::Token token(T_LNUMBER, "23", "test", 0);
+    printf(
+        "type:%d\n"
+        "entity:%s\n"
+        "moduleName:%s\n"
+        "line:%d\n",
+        token.getType(), 
+        token.getEntity().c_str(), 
+        token.getModuleName().c_str(), 
+        token.getLine());
+    return 0;
+}
